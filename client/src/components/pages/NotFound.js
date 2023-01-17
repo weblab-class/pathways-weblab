4214710273458bbd49c5f61fdcd7af4bd5724939
Matt Mosqueda <mattmosq@mit.edu>
2023-01-17 18:09:30 -0500

    Put a sad face in the 404 pagelmao

--- client/src/components/pages/NotFound.js
+++ client/src/components/pages/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div>
       <h1>404 Not Found</h1>
-      <p>The page you requested couldn't be found.</p>
+      <p>The page you requested couldn't be found :().</p>
     </div>
   );
 };
