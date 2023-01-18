90c7ec23cc52528cf3c32022ad37419b2b2b4a43
Matt Mosqueda <mattmosq@mit.edu>
2023-01-18 18:27:25 -0500

    About.js skeleton code

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 const About = () => {
-  <div>
-    <h1>Our About Section</h1>
-    <p> This is the about section.</p>
-  </div>;
+  return (
+    <div>
+      <h1>Our About Section</h1>
+      <p> This is the about section.</p>
+    </div>
+  );
 };
