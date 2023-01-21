a963fd920817866e4d94edde6e6c8bb383f65ed3
Matt Mosqueda <mattmosq@mit.edu>
2023-01-20 22:57:23 -0500

    last of the problems

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -44,7 +44,7 @@ const App = () => {
 
   return (
     <>
-      {<NavBar handleLogin={handleLogin} handleLogout={handleLogout} userId={userId} />}
+      {/* {<NavBar handleLogin={handleLogin} handleLogout={handleLogout} userId={userId} />} */}
       <Router>
         <Skeleton path="/" handleLogin={handleLogin} handleLogout={handleLogout} userId={userId} />
         <NotFound default />
