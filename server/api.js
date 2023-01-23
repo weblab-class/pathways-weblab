291fe130942bf2d6b8ac19543619e0b15767d884
Paul Sizaire <psizaire@outlook.com>
2023-01-22 20:06:43 -0500

    Creation of input and result schemas. Creation of 3 API endpoints: get all projects, get one project, and post one project. Creation of empty calculation file

--- server/api.js
+++ server/api.js
@@ -12,6 +12,8 @@ const express = require("express");
 // import models so we can interact with the database
 const User = require("./models/user");
 const Project = require("./models/project");
+const Input = require("./models/inputs");
+const Result = require("./models/results");
 
 // import authentication library
 const auth = require("./auth");
@@ -49,5 +51,49 @@ router.all("*", (req, res) => {
   console.log(`API route not found: ${req.method} ${req.url}`);
   res.status(404).send({ msg: "API route not found" });
 });
+
+// Get all projects
+router.get("/projects", (req, res) => {
+  try {
+    Project.find({}).then((projects) => res.send(projects));
+  } catch (error) {
+    console.log(error)
+  };
+}
+);
+
+// Get specific project information
+router.get("/project", (req, res) => {
+  try {
+    Project.find({ project_id: req.query._id }).then((project) => res.send(project));
+  } catch (error) {
+    console.log(error)
+  };
+}
+);
+
+// Create a new project
+router.post("project", (req, res) => {
+  try {
+    const newProject = new Project({
+      project_name: req.body.project_name,
+      project_type: req.body.project_type,
+      creator_id: req.user._id,
+      location: {
+        address: req.body.location.address,
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  };
+}
+);
+
+
+
+// Input data into a project
+
+
+
 
 module.exports = router;
