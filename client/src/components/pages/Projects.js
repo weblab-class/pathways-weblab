f56e8e1d7ebae17bdf384da165fa7d36c77cdbb6
Matt Mosqueda <mattmosq@mit.edu>
2023-01-31 18:18:06 -0500

    css for modal

--- client/src/components/pages/Projects.js
+++ client/src/components/pages/Projects.js
@@ -17,8 +17,9 @@ const customStyles = {
     bottom: "auto",
     marginRight: "-50%",
     transform: "translate(-50%, -50%)",
+    borderRadius: "25px",
     backgroundColor: "white",
-    width: 400,
+    width: "25rem",
     height: 400,
   },
 };
@@ -37,7 +38,7 @@ const Projects = (props) => {
     get("/api/projects", { creator_id: props.userId }).then((projectObj) => {
       setProjects(projectObj);
     });
-  }, [props.userId, projects])
+  }, [props.userId, projects]);
 
   // called whenever the user types in the name input box
   const handleNameChange = (event) => {
@@ -62,8 +63,8 @@ const Projects = (props) => {
 
   // called whenever the user changes the project type
   const handleProjectTypeChange = (event) => {
-    setProjectType(event.target.value)
-  }
+    setProjectType(event.target.value);
+  };
 
   // Create a project
   let navigate = useNavigate();
@@ -73,19 +74,17 @@ const Projects = (props) => {
       project_type: projectType,
       picture: projectPicture,
       location_city: projectCity,
-      location_country: projectCountry
+      location_country: projectCountry,
     };
     post("/api/project", body).then((project) => {
-      navigate("/inputs", { state: { project_id: project._id, user_id: props.userId } })
+      navigate("/inputs", { state: { project_id: project._id, user_id: props.userId } });
     });
   };
 
   // Navigate to input page when a project is created
 
-
   let myProjectsList = null;
   const hasProjects = projects.length !== 0;
-
 
   if (hasProjects) {
     myProjectsList = projects.map((projectObj) => (
@@ -97,31 +96,35 @@ const Projects = (props) => {
         picture={projectObj.picture}
         location_city={projectObj.location_city}
         location_country={projectObj.location_country}
-
       />
-    ))
+    ));
   } else {
     myProjectsList = <div className="Start-Project">Start a Project!</div>;
   }
 
   return (
     <div>
-      {
-        props.userId ?
-          <div className="ProjBox">
-            <div className="App">
-              <button className="Create-Project-Button" onClick={setModalOpen}>Create Project</button>
-              <Modal
-                isOpen={modalOpen}
-                onRequestClose={() => setModalOpen(false)}
-                style={customStyles}
-              >
-                <h2>NEW PROJECT</h2>
-                <button onClick={() => setModalOpen(false)}>X</button>
+      {props.userId ? (
+        <div className="ProjBox">
+          <div className="App">
+            <button className="Create-Project-Button" onClick={setModalOpen}>
+              Create Project
+            </button>
+            <Modal
+              className="modal"
+              isOpen={modalOpen}
+              onRequestClose={() => setModalOpen(false)}
+              style={customStyles}
+            >
+              <button className="exit-button" onClick={() => setModalOpen(false)}>
+                X
+              </button>
+              <h2 className="new-project-header">New Project</h2>
 
-
-                <div>Input a Project Name:</div>
-                <input type="text"
+              <div className="input-project-headers">Input a Project Name:</div>
+              <div className="input-project-user">
+                <input
+                  type="text"
                   name="Project Name Input"
                   id="favourite"
                   placeholder="Name of project here"
@@ -129,24 +132,23 @@ const Projects = (props) => {
                   onChange={handleNameChange}
                   size="20"
                 />
+              </div>
+              <div className="input-project-headers">Select a Project Type:</div>
+              <div className="input-project-user">
+                <select id="myList" value={projectType} onChange={handleProjectTypeChange}>
+                  <option> --- Select --- </option>
+                  <option> Table </option>
+                  <option> Garden Pot </option>
+                  <option> Lamp </option>
+                  <option> Kitchen Appliance </option>
+                  <option> Backyard Shed </option>
+                </select>
+              </div>
 
-
-                <div>Select a Project Type:</div>
-                <div>
-                  <select id="myList"
-                    value={projectType}
-                    onChange={handleProjectTypeChange} >
-                    <option> ---Select--- </option>
-                    <option> Table </option>
-                    <option> Garden Pot </option>
-                    <option> Lamp </option>
-                    <option> Kitchen Appliance </option>
-                    <option> Backyard Shed </option>
-                  </select>
-                </div>
-
-                <div>Input a Project Location:</div>
-                <input type="text"
+              <div className="input-project-headers">Input a Project Location:</div>
+              <div className="input-project-user">
+                <input
+                  type="text"
                   name="City Input"
                   id="favourite"
                   placeholder="City"
@@ -154,7 +156,10 @@ const Projects = (props) => {
                   onChange={handleCityChange}
                   size="20"
                 />
-                <input type="text"
+              </div>
+              <div className="input-project-user">
+                <input
+                  type="text"
                   name="Country Input"
                   id="favourite"
                   placeholder="Country"
@@ -162,9 +167,11 @@ const Projects = (props) => {
                   onChange={handleCountryChange}
                   size="20"
                 />
-
-                <div>Add a Picture:</div>
-                <input type="text"
+              </div>
+              <div className="input-project-headers">Add a Picture:</div>
+              <div className="input-project-user">
+                <input
+                  type="text"
                   name="projectPicture"
                   id="favourite"
                   placeholder="Paste a URL of a picture here"
@@ -172,47 +179,53 @@ const Projects = (props) => {
                   onChange={handlePictureChange}
                   size="20"
                 />
-
-
+              </div>
+              <div className="delete-create-buttons">
                 <button onClick={() => setModalOpen(false)}>Delete</button>
-                <button onClick={() => { createProject() }}>Create</button>
+                <button
+                  onClick={() => {
+                    createProject();
+                  }}
+                >
+                  Create
+                </button>
                 {/* <Link to="/inputs/" className="Create-Final" onClick={() => { createProject() }}>Create</Link>*/}
-              </Modal>
-            </div>
-            <h1 className="ProjTitle">My Projects</h1>
-            <div className="HorizontalLine"></div>
-            <div className = "CardsList">{myProjectsList}</div>
-            <h1 className="ProjTitle">Templates</h1>
-            <div className="HorizontalLine"></div>
-            <Card
-              project_name="Template"
-              project_id="63d330ca1007a20f205f9d61"
-              location_city={"Havana"}
-              location_country={"Cuba"}
-              project_type = {"Table"}
-              picture={"https://cdn-images.article.com/products/SKU379A/2890x1500/image81730.jpg"}
-            />
+              </div>
+            </Modal>
           </div>
-          :
-          <div className="ProjBox">
-            <h1 className="ProjTitle">My Projects</h1>
-            <div className="HorizontalLine"></div>
-            <div className="startProject">Login to create a project!</div>
-            <h1 className="ProjTitle">Templates</h1>
-            <div className="HorizontalLine"></div>
-            <Card
-              project_name="Template"
-              project_id="63d330ca1007a20f205f9d61"
-              creator_id="TEMPLATE"
-              location_city={"Havana"}
-              location_country={"Cuba"}
-              project_type = {"Table"}
-              picture={"https://cdn-images.article.com/products/SKU379A/2890x1500/image81730.jpg"}
-            />
-          </div>
-
-      }
+          <h1 className="ProjTitle">My Projects</h1>
+          <div className="HorizontalLine"></div>
+          <div className="CardsList">{myProjectsList}</div>
+          <h1 className="ProjTitle">Templates</h1>
+          <div className="HorizontalLine"></div>
+          <Card
+            project_name="Template"
+            project_id="63d330ca1007a20f205f9d61"
+            location_city={"Havana"}
+            location_country={"Cuba"}
+            project_type={"Table"}
+            picture={"https://cdn-images.article.com/products/SKU379A/2890x1500/image81730.jpg"}
+          />
+        </div>
+      ) : (
+        <div className="ProjBox">
+          <h1 className="ProjTitle">My Projects</h1>
+          <div className="HorizontalLine"></div>
+          <div className="startProject">Login to create a project!</div>
+          <h1 className="ProjTitle">Templates</h1>
+          <div className="HorizontalLine"></div>
+          <Card
+            project_name="Template"
+            project_id="63d330ca1007a20f205f9d61"
+            creator_id="TEMPLATE"
+            location_city={"Havana"}
+            location_country={"Cuba"}
+            project_type={"Table"}
+            picture={"https://cdn-images.article.com/products/SKU379A/2890x1500/image81730.jpg"}
+          />
+        </div>
+      )}
     </div>
-  )
+  );
 };
 export default Projects;
